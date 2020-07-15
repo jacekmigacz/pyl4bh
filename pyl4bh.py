f4008adf9fc72c11c059ef4c2caeecbb8f63ff53
Jacek Migacz <jacekmigacz@gmail.com>
2020-07-15 23:43:50 +0200

    czysty

--- pyl4bh.py
+++ pyl4bh.py
@@ -5,7 +5,6 @@ top_level_symbols = {
     '-': operator.sub,
     '*': operator.mul,
     '/': operator.floordiv,
-    't': True,
     '>=': operator.ge,
     '<=': operator.le,
 }
@@ -15,7 +14,6 @@ class Function:
     def __init__(self, arguments, body):
         self.arguments = arguments
         self.body = body
-        print(f"*FUNCTION*: args={arguments}, body={body}")
 
     def __call__(self, *arguments):
         local_symbols = dict(zip(self.arguments, arguments))
@@ -43,7 +41,6 @@ def parse(tokens):
 
 
 def evaluate(expression, local_symbols=top_level_symbols):
-    print(f"type={type(expression)}, expression={expression}, local_symbols={local_symbols}")
     if isinstance(expression, int):
         return expression
     elif isinstance(expression, str):
@@ -53,7 +50,6 @@ def evaluate(expression, local_symbols=top_level_symbols):
         top_level_symbols[NAME] = Function(ARGLIST, BODY)
     elif expression[0] == 'if':
         (_, COND, THEN, ELSE) = expression
-        print(f"COND={COND}, THEN={THEN}, ELSE={ELSE}")
         return evaluate(THEN if evaluate(COND, local_symbols) else ELSE, local_symbols)
     else:
         symbol = local_symbols.get(expression[0])
@@ -61,13 +57,9 @@ def evaluate(expression, local_symbols=top_level_symbols):
         return symbol(*arguments)
 
 
-# text = "(defun mypow(x)(* x x))(+ 1 (mypow (* 10 (mypow 40))))"
-# text = "(if t 10 1)"
-text = "(if (<= 10 9) 1 0)"
+text = "(defun mypow(x)(* x x))(+ 1 (mypow (* 10 (mypow (if (<= 44 33) 10 20)))))"
 tokens = tokenize(text)
-print(f"tokens: {tokens}")
 while tokens:
     ast = parse(tokens)
-    print(f"AST: {ast}")
     retval = evaluate(ast)
 print(retval)
