87bbbbd0b22b218fa251d20a8d3fbd8c4a96122e
Jacek Migacz <jacekmigacz@gmail.com>
2020-07-16 18:34:18 +0200

    alu and tests

--- pyl4bh.py
+++ pyl4bh.py
@@ -1,16 +1,22 @@
 import operator
+import unittest
 
 top_level_symbols = {
-    '+': operator.add,
-    '-': operator.sub,
-    '*': operator.mul,
-    '/': operator.floordiv,
-    '>=': operator.ge,
-    '<=': operator.le,
+    '+':   operator.add,
+    '-':   operator.sub,
+    '*':   operator.mul,
+    '/':   operator.floordiv,
+    '>=':  operator.ge,
+    '<=':  operator.le,
+    'and': operator.and_,
+    'or':  operator.or_,
+    'xor': operator.xor,
+    'not': operator.not_,
 }
 
 
 class Function:
+    """Abstract LISP function with variable-length argument list."""
     def __init__(self, arguments, body):
         self.arguments = arguments
         self.body = body
@@ -22,10 +28,26 @@ class Function:
 
 
 def tokenize(text):
+    """Chunk stream of characters into a series of words and symbols.
+
+    Args:
+        text (str): LISP source code.
+
+    Returns:
+        Vector of words and symbols.
+    """
     return text.replace('(', ' ( ').replace(')', ' ) ').split()
 
 
 def parse(tokens):
+    """Build an AST that mirrors the nested nature of the grammar.
+
+    Args:
+        tokens ([]): Vector of words and symbols.
+
+    Returns:
+        The abstract syntax tree (AST).
+    """
     token = tokens.pop(0)
     if token == '(':
         branch = []
@@ -41,6 +63,15 @@ def parse(tokens):
 
 
 def evaluate(expression, local_symbols=top_level_symbols):
+    """Evaluate abstract syntax tree.
+
+    Args:
+        expression ([]): AST branch.
+        local_symbols ({}, optional): Dictionary of symbols.
+
+    Returns:
+        AST's root evaluation result.
+    """
     if isinstance(expression, int):
         return expression
     elif isinstance(expression, str):
@@ -57,9 +88,38 @@ def evaluate(expression, local_symbols=top_level_symbols):
         return symbol(*arguments)
 
 
-text = "(defun mypow(x)(* x x))(+ 1 (mypow (* 10 (mypow (if (<= 44 33) 10 20)))))"
-tokens = tokenize(text)
-while tokens:
-    ast = parse(tokens)
-    retval = evaluate(ast)
-print(retval)
+def lisp(text):
+    """Run minimal LISP interpreter.
+
+    Args:
+        text (str): LISP source code.
+
+    Returns:
+        AST's root evaluation result.
+    """
+    tokens = tokenize(text)
+    while tokens:
+        ast = parse(tokens)
+        retval = evaluate(ast)
+    return retval
+
+
+class TestLisp(unittest.TestCase):
+    def test_boolean(self):
+        self.assertEqual(lisp("(+ (and 1 1) (+ (and 0 1) (+ (and 1 0) (and 0 0))))"), 1)
+        self.assertEqual(lisp("(+ (or 1 1) (+ (or 0 1) (+ (or 1 0) (or 0 0))))"), 3)
+        self.assertEqual(lisp("(+ (xor 1 1) (+ (xor 0 1) (+ (xor 1 0) (xor 0 0))))"), 2)
+        self.assertEqual(lisp("(+ (not 1) (not 0))"), 1)
+
+    def test_arithmetic(self):
+        self.assertEqual(lisp("(/ 99999 (- (+ 10 20) (* 78 11)))"), -121)
+
+    def test_defun(self):
+        self.assertEqual(lisp("(defun mypow2(x)(* x x))(+ 1 (mypow2 (* 10 (mypow2 (if (<= 44 33) 10 20)))))"), 16000001)
+
+    def test_recursion(self):
+        self.assertEqual(lisp("(defun f(x) (if (<= x 4) x (f(- x 1))))(f (* 10 10))"), 4)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
